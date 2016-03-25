d74e9c6ac8fd6df8eadd2a52c507ba0e47ca8555
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2016-03-25 18:31:38 +0100

    Files

---
+++ PostiveDaily.js
@@ -0,0 +1,51 @@
+
+var start = + new Date();
+var PositiveDaily = {
+
+	triggerWords: /\b(WAR?|DONALD TRUMP|SAD|EMOTIONAL|TERRORIST|ISLAMIST|DIES|KILL?)\b/i,
+	articleNames: ["li", ".post",".article",".story", ".g", ".zergentity", ".unit", ".lede", ".item", ".item-box", ".archive-item", ".news", ".entry", ".highlight"],
+
+	checkClass: function(element) {
+		if ($(element).hasClass("checked")) {
+			return false
+		}
+		else {
+			return true;
+		}
+	},
+
+	findNegatives: function(element) {
+		var containsNegatives = false;
+		var searchThis = $(element).text();
+
+		if (searchThis != undefined) {
+			if (searchThis.match(PositiveDaily.triggerWords) != null) {
+				containsNegatives = true;
+			}
+		}
+
+		return containsNegatives;
+	},
+
+
+
+	eraseNegatives: function() {
+
+		this.articleNames.forEach(function(name) {
+			$(name  + ':not(.checked)').addClass("checked").filter(function() {
+				return PositiveDaily.findNegatives(this);
+			}).css("display", "none")
+		})
+	},
+
+	blockNegatives: function() {
+		setInterval(this.eraseNegatives.bind(this), 1000);
+		this.eraseNegatives();
+	}
+
+
+}
+
+PositiveDaily.blockNegatives();
+var end = + new Date();
+console.log(end-start);
